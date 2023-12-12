ff29b46d53e71e47eb8cd7d23f9f7e3698b19364
Jonas Troeltsch <jonas.troeltsch@heig-vd.ch>
2023-12-12 19:39:29 +0100

    refactor emitters

--- src/main/java/ch/heigvd/emitters/Archer.java
+++ src/main/java/ch/heigvd/emitters/Archer.java
@@ -2,68 +2,8 @@ package ch.heigvd.emitters;
 
 import picocli.CommandLine;
 
-import java.io.IOException;
-import java.net.*;
-import java.nio.charset.StandardCharsets;
-import java.util.concurrent.Executors;
-import java.util.concurrent.ScheduledExecutorService;
-import java.util.concurrent.TimeUnit;
-
 @CommandLine.Command(
         name = "archer",
         description = "Start an UDP multicast emitter representing an archer"
 )
-public class Archer extends Enemy {
-    @CommandLine.Option(
-            names = {"-i", "--interface"},
-            description = "Interface to use.",
-            scope = CommandLine.ScopeType.INHERIT,
-            required = true
-    )
-    private String interfaceName;
-
-    @Override
-    public Integer call() {
-        try (MulticastSocket socket = new MulticastSocket(parent.getPort())) {
-            String myself = InetAddress.getLocalHost().getHostAddress() + ":" + parent.getPort();
-            System.out.println("Multicast emitter started (" + myself + ")");
-
-            InetAddress multicastAddress = InetAddress.getByName(host);
-            InetSocketAddress group = new InetSocketAddress(multicastAddress, parent.getPort());
-            NetworkInterface networkInterface = NetworkInterface.getByName(interfaceName);
-            socket.joinGroup(group, networkInterface);
-
-            ScheduledExecutorService scheduler = Executors.newScheduledThreadPool(1);
-            scheduler.scheduleAtFixedRate(() -> {
-                try {
-                    String message = "Hello, from multicast emitter! (" + myself + ")";
-
-                    System.out.println("Multicasting '" + message + "' to " + host + ":" + parent.getPort() + " on interface " + interfaceName);
-
-                    byte[] payload = message.getBytes(StandardCharsets.UTF_8);
-
-                    DatagramPacket datagram = new DatagramPacket(
-                            payload,
-                            payload.length,
-                            group
-                    );
-
-                    socket.send(datagram);
-                } catch (IOException e) {
-                    throw new RuntimeException(e);
-                }
-            }, pause, damage, TimeUnit.MILLISECONDS);
-
-            // Keep the program running for a while
-            scheduler.awaitTermination(Long.MAX_VALUE, TimeUnit.MILLISECONDS);
-
-            socket.leaveGroup(group, networkInterface);
-        } catch (Exception e) {
-            e.printStackTrace();
-            return 1;
-        }
-
-        return 0;
-    }
-
-}
+public class Archer extends Enemy { }
--- src/main/java/ch/heigvd/emitters/Enemy.java
+++ src/main/java/ch/heigvd/emitters/Enemy.java
@@ -2,22 +2,19 @@ package ch.heigvd.emitters;
 
 import picocli.CommandLine;
 
+import java.io.IOException;
+import java.net.*;
+import java.nio.charset.StandardCharsets;
 import java.util.concurrent.Callable;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 
 abstract public class Enemy implements Callable<Integer> {
     @CommandLine.ParentCommand
     protected ch.heigvd.Main parent;
-
     @CommandLine.Option(
-            names = {"-H", "--host"},
-            description = "Subnet range/multicast address to use.",
-            required = true,
-            scope = CommandLine.ScopeType.INHERIT
-    )
-    protected String host;
-
-    @CommandLine.Option(
-            names = {"-p", "--pause"},
+            names = {"-pa", "--pause"},
             description = "Pause between attacks (in milliseconds) (default: 10000).",
             defaultValue = "10000"
     )
@@ -30,4 +27,56 @@ abstract public class Enemy implements Callable<Integer> {
     )
     protected int damage;
 
+    @CommandLine.Option(
+            names = {"-i", "--interface"},
+            description = "Interface to use.",
+            scope = CommandLine.ScopeType.INHERIT,
+            required = true
+    )
+    protected String interfaceName;
+
+
+    @Override
+    public Integer call() {
+        try (MulticastSocket socket = new MulticastSocket(parent.getPort())) {
+            String myself = InetAddress.getLocalHost().getHostAddress() + ":" + parent.getPort();
+            System.out.println("Archer Multicast emitter started (" + myself + ")");
+
+            InetAddress multicastAddress = InetAddress.getByName(parent.getHost());
+            InetSocketAddress group = new InetSocketAddress(multicastAddress, parent.getPort());
+            NetworkInterface networkInterface = NetworkInterface.getByName(interfaceName);
+            socket.joinGroup(group, networkInterface);
+
+            ScheduledExecutorService scheduler = Executors.newScheduledThreadPool(1);
+            scheduler.scheduleAtFixedRate(() -> {
+                try {
+                    String message = "ATTACK " + this.getClass().getSimpleName() + " " + damage;
+
+                    System.out.println("Multicasting '" + message + "' to " + parent.getHost() + ":" + parent.getPort() + " on interface " + interfaceName);
+
+                    byte[] payload = message.getBytes(StandardCharsets.UTF_8);
+
+                    DatagramPacket datagram = new DatagramPacket(
+                            payload,
+                            payload.length,
+                            group
+                    );
+
+                    socket.send(datagram);
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }, pause, pause, TimeUnit.MILLISECONDS);
+
+            // Keep the program running for a while
+            scheduler.awaitTermination(Long.MAX_VALUE, TimeUnit.MILLISECONDS);
+
+            socket.leaveGroup(group, networkInterface);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return 1;
+        }
+
+        return 0;
+    }
 }
