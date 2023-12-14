72fc7c35e8841bd0bb890e2f70c0eed05a78d4fe
Jonas Troeltsch <jonas.troeltsch@heig-vd.ch>
2023-12-14 14:20:14 +0100

    add a basic TowerDefence class for game logic

---
+++ src/main/java/ch/heigvd/server/TowerDefence.java
@@ -0,0 +1,39 @@
+package ch.heigvd.server;
+
+import lombok.Getter;
+import lombok.Synchronized;
+
+public class TowerDefence {
+    @Getter(onMethod_={@Synchronized})
+    private int hp;
+    private int protection;
+
+    TowerDefence(int baseHP, int baseProtection) {
+        this.hp = baseHP;
+        this.protection = baseProtection;
+    }
+
+    /**
+     * @param damage damage to take
+     * @return new tower hp
+     */
+    public synchronized int takeDamage(int damage) {
+        return (hp -= (int) (damage * (1 - protection / 100.0)));
+    }
+
+    /**
+     * @param heal hp to add
+     * @return new tower hp
+     */
+    public synchronized int heal(int heal) {
+        return (hp += heal);
+    }
+
+    /**
+     * @param protection protection to add
+     */
+    public synchronized void addProtection(int protection) {
+        this.protection += protection;
+    }
+
+}
