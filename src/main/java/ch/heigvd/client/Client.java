ac22d2acbc42175c5d3bb229e7feaa1ce48d748a
Jonas Troeltsch <jonas.troeltsch@heig-vd.ch>
2023-12-18 21:20:55 +0100

    rebase wip

--- src/main/java/ch/heigvd/client/Client.java
+++ src/main/java/ch/heigvd/client/Client.java
@@ -5,15 +5,16 @@ import ch.heigvd.utils.ProtectionType;
 import ch.heigvd.utils.Utils;
 import picocli.CommandLine;
 
+import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
+import java.nio.charset.StandardCharsets;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Scanner;
 import java.util.concurrent.Callable;
 
 import static ch.heigvd.utils.MessageType.getByDimOrName;
-import static ch.heigvd.utils.Utils.send;
 import static java.lang.System.exit;
 
 @CommandLine.Command(name = "client", description = "Starts a client for a game of Tower Defense")
@@ -54,7 +55,7 @@ public class Client implements Callable<Integer> {
         return 0;
     }
 
-    public Integer waitUserInput() throws RuntimeException {
+    public void waitUserInput() throws RuntimeException {
         try (DatagramSocket socket = new DatagramSocket()) {
             String myself = InetAddress.getLocalHost().getHostAddress() + ":" + port;
             System.out.println("Client emitter started (" + myself + ")");
@@ -68,48 +69,39 @@ public class Client implements Callable<Integer> {
             String[] commandSplit;
             String timestamp;
             //chaque fois qu'on send on attend la réponse sur LE MEME DATAGRAM envoyé on est donc seulement un emetteur
-            while (true)
-            {
+            while (true) {
                 System.out.println("listen client console input :" + port);
                 command = sc.nextLine().toUpperCase(); // listen user input
                 timestamp = dateFormat.format(new Date());
                 commandSplit = command.split(" ");
 
-                if (commandSplit.length == 0 || !MessageType.isIn(commandSplit[0]))
-                {
-                    if (    commandSplit[0].equalsIgnoreCase("HELP") ||
-                            commandSplit[0].equalsIgnoreCase("H"))
-                    {
+                if (commandSplit.length == 0 || !MessageType.isIn(commandSplit[0])) {
+                    if (commandSplit[0].equalsIgnoreCase("HELP") ||
+                            commandSplit[0].equalsIgnoreCase("H")) {
                         System.out.println("Help Command Menu :");
                         System.out.println("h, H, help, HELP : this menu");
                         System.out.println("pro heal 15 : heal with 15");
                         System.out.println("pro defend 15 : defend with 15");
                         System.out.println("get, GET_INFO : request tower information");
                         continue;
-                    }
-                    else if (   commandSplit[0].equalsIgnoreCase("LEAVE") ||
-                                commandSplit[0].equalsIgnoreCase("L")) {
+                    } else if (commandSplit[0].equalsIgnoreCase("LEAVE") ||
+                            commandSplit[0].equalsIgnoreCase("L")) {
                         System.out.println("Leaving...");
                         exit(0);
                     }
                 }
                 messageType = getByDimOrName(commandSplit[0]);
 
-                if (messageType == null)
-                {
+                if (messageType == null) {
                     message = MessageType.DEFAULT + " Hello, from '" + myself + "' not a command : <" + command + "> at " + timestamp + ")";
-                    send(message, timestamp, serverAddress, socket,host,port);
+                    send(message, timestamp, serverAddress, socket, host, port);
                     continue;
                 }
-                switch (messageType)
-                {
-                    case PROTECT :
-                        if (commandSplit.length == 3 && ProtectionType.isIn(commandSplit[1]) && Utils.isNumeric(commandSplit[2]))
-                        {
+                switch (messageType) {
+                    case PROTECT:
+                        if (commandSplit.length == 3 && ProtectionType.isIn(commandSplit[1]) && Utils.isNumeric(commandSplit[2])) {
                             message = messageType.name() + " " + ProtectionType.getByDimOrName(commandSplit[1]) + " " + commandSplit[2];
-                        }
-                        else
-                        {
+                        } else {
                             message = messageType.name() + " " + ProtectionType.HEAL + " " + defaultValue;
                             System.out.println("Command error, send " + message + " instead of : " + command);
                         }
@@ -125,11 +117,75 @@ public class Client implements Callable<Integer> {
 
                 }
 
-                send(message, timestamp, serverAddress, socket,host,port);
+                send(message, timestamp, serverAddress, socket, host, port);
             }
         } catch (Exception e) {
             e.printStackTrace();
-            return 1;
+            throw new RuntimeException(e);
+        }
+    }
+
+    public void send(String message, String timestamp, InetAddress serverAddress, DatagramSocket socket, String host, int port) throws Exception {
+        System.out.println("Unicasting '" + message + "' to " + host + ":" + port + " at " + timestamp);
+
+        byte[] payload = message.getBytes(StandardCharsets.UTF_8);
+
+        DatagramPacket datagram = new DatagramPacket(
+                payload,
+                payload.length,
+                serverAddress,
+                port
+        );
+        System.out.print("sending... ");
+        socket.send(datagram); // send data to serv
+        System.out.println("data sended !");
+
+        socket.receive(datagram);
+        message = new String(
+                datagram.getData(),
+                datagram.getOffset(),
+                datagram.getLength(),
+                StandardCharsets.UTF_8
+        );
+        MessageType messageType = MessageType.findByName(message.split(" ")[0]);
+        System.out.println("RECEIVED :" + message);
+        if (messageType == null) {
+            System.out.println("received message not handled : " + message);
+            return;
+        }
+        System.out.println("received :-" + message + "-"); // TODO : remove after tests (tmp debug)
+        String[] splitMessage = message.split(" ");
+
+        switch (messageType) {
+            case ANSWER:
+                if (splitMessage.length <= 1 || splitMessage[1].isEmpty()) {
+                    System.out.println("[SERVER ERROR] : server give 0 info ?");
+                } else {
+                    System.out.print("[TOWER INFO] :");
+                    for (int i = 1; i < splitMessage.length; ++i) {
+                        System.out.print(" ");
+                        System.out.print(splitMessage[i]);
+                    }
+                    System.out.println();
+                }
+                break;
+
+            case GAME_LOST:
+                System.out.println("[TOWER INFO] : GAME OVER X_X (presse enter to leave)");
+                sc.nextLine();
+                exit(0);
+                break;
+
+            case ERROR:
+                System.out.println("[SERVER ERROR] : invalide commande :`" + message + "`");
+                break;
+
+            case PROTECT:
+            case GET_INFO:
+            case ATTACK:
+            case DEFAULT:
+                // ne devrait pas arriver
+                break;
         }
     }
 }
--- src/main/java/ch/heigvd/server/AlliesWorker.java
+++ src/main/java/ch/heigvd/server/AlliesWorker.java
@@ -2,8 +2,8 @@ package ch.heigvd.server;
 
 import ch.heigvd.utils.MessageType;
 import ch.heigvd.utils.ProtectionType;
-import java.text.SimpleDateFormat;
-import java.util.Date;
+
+import java.io.IOException;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
@@ -12,115 +12,135 @@ import java.util.concurrent.Callable;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
-import static ch.heigvd.utils.Utils.send;
+import static ch.heigvd.utils.MessageType.ANSWER;
 
 public class AlliesWorker implements Callable<Integer> {
 
-        // This is new - could be passed as a parameter with picocli
+    // This is new - could be passed as a parameter with picocli
+    private static TowerDefense tower;
+
+    private static final int NUMBER_OF_THREADS = 1;
+    private ch.heigvd.Main parent;
+    static private int port;
+    static private String host;
+
+    public AlliesWorker(TowerDefense tower, ch.heigvd.Main parent, int portu, String hostu) {
+        this.tower = tower;
+        this.parent = parent;
+        this.port = portu;
+        host = hostu;
+
+    }
+
+    @Override
+    public Integer call() {
+        // This is new - we define an executor service
+        ExecutorService executor = null;
+
+        try (DatagramSocket socket = new DatagramSocket(port)) {
+            // This is new - the executor service has a pool of threads
+            executor = Executors.newFixedThreadPool(NUMBER_OF_THREADS);
+
+            String myself = InetAddress.getLocalHost().getHostAddress() + ":" + port;
+            System.out.println("Unicast receiver started (" + myself + ")");
+
+            byte[] receiveData = new byte[1024];
+
+            while (true) {
+                DatagramPacket packet = new DatagramPacket(
+                        receiveData,
+                        receiveData.length
+                );
+
+                socket.receive(packet);
+
+                // This is new - we submit a new task to the executor service
+                executor.submit(new ClientHandler(packet, myself, tower, socket));
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            return 1;
+        }
+    }
+
+    // This is new - we define a new class to handle the client
+    static class ClientHandler implements Runnable {
+
+        private final DatagramPacket packet;
+        private final String myself;
+
         private TowerDefense tower;
+        private DatagramSocket socket;
 
-        private static final int NUMBER_OF_THREADS = 1;
-        private ch.heigvd.Main parent;
-        static private int port;
-        static private String host;
-
-        public AlliesWorker(TowerDefense tower,ch.heigvd.Main parent, int portu, String hostu) {
+        public ClientHandler(DatagramPacket packet, String myself, TowerDefense tower, DatagramSocket socket) {
+            this.packet = packet;
+            this.myself = myself;
             this.tower = tower;
-            this.parent = parent;
-            this.port = portu;
-            host = hostu;
-
+            this.socket = socket;
         }
 
         @Override
-        public Integer call() {
-            // This is new - we define an executor service
-            ExecutorService executor = null;
+        public void run() {
+            String message = new String(
+                    packet.getData(),
+                    packet.getOffset(),
+                    packet.getLength(),
+                    StandardCharsets.UTF_8
+            );
 
-            try (DatagramSocket socket = new DatagramSocket(port)) {
-                // This is new - the executor service has a pool of threads
-                executor = Executors.newFixedThreadPool(NUMBER_OF_THREADS);
+            System.out.println("Unicast receiver (" + myself + ") received message: " + message);
 
-                String myself = InetAddress.getLocalHost().getHostAddress() + ":" + port;
-                System.out.println("Unicast receiver started (" + myself + ")");
+            //aucun autre check parce qu'on a codé le client ou bien quand même ?
+            String messageToSend = parseMessage(message);
+            //nouveau paquet à renvoyer à la personne qui va expédier
+            byte[] payload = message.getBytes(StandardCharsets.UTF_8);
 
-                byte[] receiveData = new byte[1024];
-
-                while (true) {
-                    DatagramPacket packet = new DatagramPacket(
-                            receiveData,
-                            receiveData.length
-                    );
-
-                    socket.receive(packet);
-
-                    // This is new - we submit a new task to the executor service
-                    executor.submit(new ClientHandler(packet, myself, tower, socket));
-                }
-            } catch (Exception e) {
-                e.printStackTrace();
-                return 1;
-            }
-        }
-
-        // This is new - we define a new class to handle the client
-        static class ClientHandler implements Runnable {
-
-            private final DatagramPacket packet;
-            private final String myself;
-
-            private TowerDefense tower;
-            private DatagramSocket socket;
-
-            public ClientHandler(DatagramPacket packet, String myself, TowerDefense tower, DatagramSocket socket) {
-                this.packet = packet;
-                this.myself = myself;
-                this.tower = tower;
-                this.socket = socket;
-            }
-
-            @Override
-            public void run() {
-                String message = new String(
-                        packet.getData(),
-                        packet.getOffset(),
-                        packet.getLength(),
-                        StandardCharsets.UTF_8
-                );
-
-                System.out.println("Unicast receiver (" + myself + ") received message: " + message);
-                String timestamp;
-                SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-                //aucun autre check parce qu'on a codé le client ou bien quand même ?
-
-                if(MessageType.PROTECT  == MessageType.findByName(message.split(" ")[0])){
-                    ProtectionType proType = ProtectionType.valueOf(message.split(" ")[1]);
-                    int amount = Integer.parseInt(message.split(" ")[2]);
-
-                    switch (proType){
-                        case DEFEND  :
-                            tower.addProtection(amount);
-                        case HEAL:
-                            tower.heal(amount);
-                    }
-                } else if (MessageType.GET_INFO == MessageType.findByName(message)) {
-                    timestamp = dateFormat.format(new Date());
-                    try {
-                        send(tower.toString(), timestamp, InetAddress.getByName(host), socket,host ,port); // nos adresses
-                    } catch (Exception e) {
-                        throw new RuntimeException(e);
-                    }
-                }
-                //nouveau paquet à renvoyer à la personne qui va expédier
-                String response = "RESPONSE";
-                byte[] payload = message.getBytes(StandardCharsets.UTF_8);
-                DatagramPacket datagram = new DatagramPacket(
-                        payload,
-                        payload.length,
-                        packet.getAddress(),
-                        packet.getPort() //connection bidirectionnelle dans ce contexte là grâce
-                        // à l'information de l'expéditeur contenu dans le datagram
-                );
+            DatagramPacket datagram = new DatagramPacket(
+                    payload,
+                    payload.length,
+                    packet.getAddress(),
+                    packet.getPort() //connection bidirectionnelle dans ce contexte là grâce
+                    // à l'information de l'expéditeur contenu dans le datagram
+            );
+            try {
+                socket.send(datagram);
+            } catch (IOException e) {
+                throw new RuntimeException(e);
             }
         }
     }
+
+    private static String parseMessage(String message) {
+        String response = "";
+
+        String[] splitMessage = message.split(" ");
+        MessageType messageType = MessageType.findByName(splitMessage[0]);
+
+        if (messageType == null) {
+            response = "ERROR : message type not found";
+            return response;
+        }
+
+        // TODO : check if message is valid
+        switch (messageType) {
+            case PROTECT:
+                ProtectionType protectionType = ProtectionType.findByName(splitMessage[1]);
+                if (protectionType == null) {
+                    response = "ERROR : protection type not found";
+                    return response;
+                }
+                switch (protectionType) {
+                    case DEFEND:
+                        tower.addProtection(Integer.parseInt(splitMessage[2]));
+                        break;
+                    case HEAL:
+                        tower.heal(Integer.parseInt(splitMessage[2]));
+                        break;
+                }
+            case GET_INFO:
+                break;
+        }
+        response = ANSWER + " " + tower.toString();
+        return response;
+    }
+}
